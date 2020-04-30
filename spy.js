a4917d6b5b3b1dac9fade44ebd1cf5a7b274cd4b
fionanicole <fnh215@nyu.edu>
2020-04-30 05:37:32 -0400

    fixed modals

--- spy.js
+++ spy.js
@@ -1,28 +1,128 @@
 // Source for To-do list: https://www.w3schools.com/howto/howto_js_todolist.asp
-
+// Source for Modal Model: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
 
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
 
 window.onload=function(){
-var openTheModal = document.getElementById("openModal");
+var openTheModalCamera = document.getElementById("openModalCamera");
+var openTheModalVideogame = document.getElementById("openModalVideogame");
+var openTheModalCameraPaintbrush = document.getElementById("openModalPaintbrush");
+var openTheModalCameraDice = document.getElementById("openModalDice");
+var openTheModalCameraKeyboard = document.getElementById("openModalKeyboard");
 var closeTheModal = document.getElementById("closeModal");
 
-document.getElementById("openModal").addEventListener("click", displayContent);
-document.getElementById("closeModal").addEventListener("click", hideContent);
+// camera clickers
+document.getElementById("openModalCamera").addEventListener("click", displayContentCamera);
+document.getElementById("closeModalCamera").addEventListener("click", hideContentCamera);
+
+// videogame clickers
+document.getElementById("openModalVideogame").addEventListener("click", displayContentVideogame);
+document.getElementById("closeModalVideogame").addEventListener("click", hideContentVideogame);
+
+// paintbrush clickers
+document.getElementById("openModalPaintbrush").addEventListener("click", displayContentPaintbrush);
+document.getElementById("closeModalPaintbrush").addEventListener("click", hideContentPaintbrush);
+
+// dice clickers
+document.getElementById("openModalDice").addEventListener("click", displayContentDice);
+document.getElementById("closeModalDice").addEventListener("click", hideContentDice);
+
+// Keyboard clickers
+document.getElementById("openModalKeyboard").addEventListener("click", displayContentKeyboard);
+document.getElementById("closeModalKeyboard").addEventListener("click", hideContentKeyboard);
 }
 
-function displayContent(){
-  var openTheModal = document.getElementById("openModal");
-  var modalInteriorLayout = document.getElementById("modalInterior")
-  openTheModal.style.display="block";
-  modalInteriorLayout.style.display="block";
-}
-
-function hideContent(){
-  var openTheModal = document.getElementById("openModal");
-  var modalInteriorLayout = document.getElementById("modalInterior")
-  var closeTheModal = document.getElementById("closeModal");
+// camera displays
+function displayContentCamera(){
+  var openTheModal = document.getElementById("openModalCamera");
+  var modalInteriorLayout = document.getElementById("modalInteriorCamera");
+  var closeTheModal = document.getElementById("closeModalCamera");
   openTheModal.style.display="none";
+  modalInteriorLayout.style.display="block";
+  closeTheModal.style.display="block";
+}
+
+function hideContentCamera(){
+  var openTheModal = document.getElementById("openModalCamera");
+  var modalInteriorLayout = document.getElementById("modalInteriorCamera")
+  var closeTheModal = document.getElementById("closeModalCamera");
+  openTheModal.style.display="block";
+  modalInteriorLayout.style.display="none";
+  closeTheModal.style.display="none";
+}
+
+// videogame displays
+function displayContentVideogame(){
+  var openTheModal = document.getElementById("openModalVideogame");
+  var modalInteriorLayout = document.getElementById("modalInteriorVideogame");
+  var closeTheModal = document.getElementById("closeModalVideogame");
+  openTheModal.style.display="none";
+  modalInteriorLayout.style.display="block";
+  closeTheModal.style.display="block";
+}
+
+function hideContentVideogame(){
+  var openTheModal = document.getElementById("openModalVideogame");
+  var modalInteriorLayout = document.getElementById("modalInteriorVideogame")
+  var closeTheModal = document.getElementById("closeModalVideogame");
+  openTheModal.style.display="block";
+  modalInteriorLayout.style.display="none";
+  closeTheModal.style.display="none";
+}
+
+// paintbrush displays
+function displayContentPaintbrush(){
+  var openTheModal = document.getElementById("openModalPaintbrush");
+  var modalInteriorLayout = document.getElementById("modalInteriorPaintbrush");
+  var closeTheModal = document.getElementById("closeModalPaintbrush");
+  openTheModal.style.display="none";
+  modalInteriorLayout.style.display="block";
+  closeTheModal.style.display="block";
+}
+
+function hideContentPaintbrush(){
+  var openTheModal = document.getElementById("openModalPaintbrush");
+  var modalInteriorLayout = document.getElementById("modalInteriorPaintbrush")
+  var closeTheModal = document.getElementById("closeModalPaintbrush");
+  openTheModal.style.display="block";
+  modalInteriorLayout.style.display="none";
+  closeTheModal.style.display="none";
+}
+
+// dice displays
+function displayContentDice(){
+  var openTheModal = document.getElementById("openModalDice");
+  var modalInteriorLayout = document.getElementById("modalInteriorDice");
+  var closeTheModal = document.getElementById("closeModalDice");
+  openTheModal.style.display="none";
+  modalInteriorLayout.style.display="block";
+  closeTheModal.style.display="block";
+}
+
+function hideContentDice(){
+  var openTheModal = document.getElementById("openModalDice");
+  var modalInteriorLayout = document.getElementById("modalInteriorDice")
+  var closeTheModal = document.getElementById("closeModalDice");
+  openTheModal.style.display="block";
+  modalInteriorLayout.style.display="none";
+  closeTheModal.style.display="none";
+}
+
+// Keyboard
+function displayContentKeyboard(){
+  var openTheModal = document.getElementById("openModalKeyboard");
+  var modalInteriorLayout = document.getElementById("modalInteriorKeyboard");
+  var closeTheModal = document.getElementById("closeModalKeyboard");
+  openTheModal.style.display="none";
+  modalInteriorLayout.style.display="block";
+  closeTheModal.style.display="block";
+}
+
+function hideContentKeyboard(){
+  var openTheModal = document.getElementById("openModalKeyboard");
+  var modalInteriorLayout = document.getElementById("modalInteriorKeyboard")
+  var closeTheModal = document.getElementById("closeModalKeyboard");
+  openTheModal.style.display="block";
   modalInteriorLayout.style.display="none";
   closeTheModal.style.display="none";
 }
