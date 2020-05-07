fd9d7daab9b33f2cbc5690a85feba569849c9eb2
fionanicole <fnh215@nyu.edu>
2020-05-07 05:41:18 -0400

    positioning buttons

--- spy.js
+++ spy.js
@@ -1,8 +1,7 @@
 // Source for To-do list: https://www.w3schools.com/howto/howto_js_todolist.asp
 // Source for Modal Model: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
-
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
-
+// MAJOR SOURCE: https://getbootstrap.com/docs/4.1/components/modal/
 
 $(document).ready(function(){
   $("#cameraModal").click(function(){
@@ -13,5 +12,29 @@ $(document).ready(function(){
 $(document).ready(function(){
   $("#videogameModal").click(function(){
     $("#myVideogameModal").modal();
+  });
+});
+
+$(document).ready(function(){
+  $("#paintbrushModal").click(function(){
+    $("#myPaintbrushModal").modal();
+  });
+});
+
+$(document).ready(function(){
+  $("#diceModal").click(function(){
+    $("#myDiceModal").modal();
+  });
+});
+
+$(document).ready(function(){
+  $("#keyboardModal").click(function(){
+    $("#myKeyboardModal").modal();
+  });
+});
+
+$(document).ready(function(){
+  $("#orangeModal").click(function(){
+    $("#myOrangeModal").modal();
   });
 });
